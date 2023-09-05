75db5055043e05c071948d299c4ba3930150be11
Jennifer <jennifer.y.hong@gmail.com>
2023-09-05 12:31:14 -0400

    started, not working

--- server/api/members.js
+++ server/api/members.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const { getMemberById, getAllMembers, createMember, updateMember, deleteMember } = require('../db/helpers/members')
 
@@ -26,6 +28,8 @@ router.get('/', async (req, res, next) => {
 // A POST request to add a new member
 router.post('/', async (req, res, next) => {
     try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
         const member = await createMember(req.body);
         res.send(member);
     } catch (error) {
@@ -50,6 +54,23 @@ router.delete('/:member_id', async (req, res, next) => {
         res.send(member);
     } catch (error) {
         next(error);
+    }
+});
+
+// LOGIN 
+router.post('/login', async (req,res) => {
+    const member = await User.findOne({ username: req.body.username });
+
+    try {
+        const match = await bcrypt.compare(req.body.password, user.password);
+        const accessToken = jwt.sign(JSON.stringify(user), process.env.TOKEN_SECRET)
+        if(match) {
+            res.json({ accessToken: accessToken });
+        } else {
+            res.json({ message: "Invalid Login" });
+        }
+    } catch (error) {
+        console.log(error)
     }
 });
 
